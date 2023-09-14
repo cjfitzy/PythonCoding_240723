941f13031aed14050ac7614f34552e71d60e9f2c
cjfitzy <cj_fitzy@hotmail.co.uk>
2023-09-14 09:54:00 +0100

    fileio update

--- Python/Day3/file.py
+++ Python/Day3/file.py
@@ -1,54 +1,10 @@
-#from dataclasses import dataclass
-
-
-#fileobjectvar = open("Python/Day3/Readme.md")
-
-#print(type(fileobjectvar))
-#stringvar = fileobjectvar.read()
-#print(stringvar)
-
-#print("how many sheep in the file")
-#print(stringvar.count("sheep"))
-
-
-
-#dogstory = stringvar.replace("sheep", "dog")
-#print(dogstory)
-
-#fileobjectvar.seek(0)
-
-#listvar1 = fileobjectvar.readlines()
-#print(listvar1[2])  # graps a line 
-
-#listvar1.append("THE END\n")
-#print(listvar1)
-
-
-#fileobjectvar.close()
-
-#writefileobjectvar = open("Python/Day3/Readme.md","w") # open and delete file contents
-
-##push the data to be written to disk
-#writefileobjectvar.write(dogstory)
-#writefileobjectvar.close()
-
-
-##basic work
-##open()
-##read() or readline()
-##close()
-##manipulate the dataclass
-##open("w")
-##write()
-##close()
-
 carsales = open("Python/Day3/carSale.csv")
 
 
 
 header = carsales.readline()
 lines = carsales.readlines()
-print(header)
+
 
 janTotal = 0
 febTotal = 0
@@ -71,39 +27,57 @@ for line in lines:
     juneTotal += int(words[6])
     julyTotal += int(words[7])
     augTotal += int(words[8])
-   # TotalSales = (str(words[0]), ("Total yearly Sales"), int(words[1]) + int(words[2]) + int(words[3]) + int(words[4]) + int(words[5]) + int(words[6]) + int(words[7]) + int(words[8]))
-    #print(TotalSales)
+    TotalSales = (str(words[0]), ("Total yearly Sales"), int(words[1]) + int(words[2]) + int(words[3]) + int(words[4]) + int(words[5]) + int(words[6]) + int(words[7]) + int(words[8]))
+    print(TotalSales)
     sales.append(words[1]+ words[2])
-    print(sales)
 print("Jan 2019 Total =", janTotal,"Feb 2019 Total =", febTotal, "March 2019 Total =", marchTotal,"April 2019 Total =", aprTotal,"May 2019 Total =", mayTotal,"June 2019 Total =", juneTotal,"July 2019 Total =", julyTotal,"August 2019 Total =", augTotal )
-print(janTotal +  febTotal + marchTotal + aprTotal + mayTotal + juneTotal + julyTotal + augTotal  )
+print("Grand Total",janTotal +  febTotal + marchTotal + aprTotal + mayTotal + juneTotal + julyTotal + augTotal  )
 
 
-#####
-
-#companies = []
-#sales = [] 
-
-#header = carsales.readline()
-#lines = carsales.readlines()
-
-##for line in lines:
-##    words1 = line.strip().split(",")
-##    sales.append(list(map(int,words1[1])))
-
-##print(sales)
 
 
-##for line in lines:
-##    words = line.split(",")
-##    print(words[1])
-##    companies.append(words[0])
-##    data = line.strip().split(',') 
-##    sales.append(list(map(int,data)))
-##    print(sales)
-#for line in lines:
-#    for x in range(0, len(lines),2): 
-#        data = line.strip().split(',') 
-#        sales.append(list(map(int,data)))
-#        print(sales)
+print("---------------------------Other way------------------- ")
+companies = []
+sales = [] 
 
+
+for line in lines:
+    parts = line.split(',')
+    company = parts[0]    
+    companies.append(company)    
+    salenumbers = [int(s) for s in parts[1:]]
+    sales.append(salenumbers)
+
+
+i = 0
+janTotal = 0
+febTotal = 0
+marchTotal = 0
+aprTotal = 0
+mayTotal = 0
+juneTotal = 0
+julyTotal = 0
+augTotal = 0
+TotalSales = 0
+
+for item in sales:
+    itemtotal = sum(item)
+    janTotal += int(item[0])
+    febTotal += int(item[1])
+    marchTotal += int(item[2])
+    aprTotal += int(item[3])
+    mayTotal += int(item[4])
+    juneTotal += int(item[5])
+    julyTotal += int(item[6])
+    augTotal += int(item[7])
+    print(companies[i],"Yearly Total:", itemtotal)
+    i += 1
+print(janTotal)
+print(febTotal)
+print(marchTotal)
+print(aprTotal)
+print(mayTotal)
+print(juneTotal)
+print(julyTotal)
+print(augTotal)
+print("Total:",janTotal + febTotal + marchTotal + aprTotal + mayTotal + juneTotal + julyTotal + augTotal )
