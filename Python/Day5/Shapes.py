b006e5b20983d46eb5450803e5519a6a6ba66712
cjfitzy <cj_fitzy@hotmail.co.uk>
2023-09-15 15:17:44 +0100

    Shapes

--- Python/Day5/Shapes.py
+++ Python/Day5/Shapes.py
@@ -18,27 +18,27 @@ class Shapes(ABC):
 class Square(Shapes):
     
     def shapeinfo(self,info):
-        return "Squares {} info".format(info)
+        return "A {} is a four-sided shape with all sides equal in length".format(info)
     def sidesinfo(self,length):
-        return "each side is {} cm".format(length)
+        return "Each side is {} cm".format(length)
 
 
 class Rectangle(Shapes):
 
     def shapeinfo(self, info):
-        return "Rectangles {} info".format(info)
+        return "A {}  is a four-sided shape with two sets of parallel sides".format(info)
 
     def sidesinfo(self,side1,side2):
-        return "Rectangles {} side1 {} side2".format(side1).format(side2)
+        return "The two longer sides are {} cm and two shorter sides are {} cm".format(side1,side2)
 
 
 squarey = Square()      
 
 
-print(squarey.shapeinfo("have 4 equal sides"))
-print(squarey.sidesinfo("4 !"))
+print(squarey.shapeinfo("Square"))
+print(squarey.sidesinfo("4"))
 
 rectangley = Rectangle()
 
-print(rectangley.shapeinfo("have 2 long equal sides and 2 short equal sides"))
-print(squarey.sidesinfo("4 !","3"))
+print(rectangley.shapeinfo("Rectangle"))
+print(rectangley.sidesinfo("4","3"))
