e006dba273dc35e8938e53118215f55e7fc1aede
cjfitzy <cj_fitzy@hotmail.co.uk>
2023-09-15 15:24:04 +0100

    add some maths into my shapes

--- Python/Day5/Shapes.py
+++ Python/Day5/Shapes.py
@@ -14,6 +14,9 @@ class Shapes(ABC):
     def sidesinfo(self):
         pass
 
+    @abstractmethod
+    def area(self):
+        pass
 
 class Square(Shapes):
     
@@ -21,6 +24,10 @@ class Square(Shapes):
         return "A {} is a four-sided shape with all sides equal in length".format(info)
     def sidesinfo(self,length):
         return "Each side is {} cm".format(length)
+    def area(self,length):
+        squarearea = int(length) * int(length)
+        return "area = {} * {} which equals " + str(squarearea) + "cm".format(length)
+        
 
 
 class Rectangle(Shapes):
@@ -30,15 +37,18 @@ class Rectangle(Shapes):
 
     def sidesinfo(self,side1,side2):
         return "The two longer sides are {} cm and two shorter sides are {} cm".format(side1,side2)
-
+    def area(self,side1,side2):
+        recarea = int(side1) * int(side2)
+        return "area = {}* {} which equals " + str(recarea) + "cm"
 
 squarey = Square()      
 
 
 print(squarey.shapeinfo("Square"))
 print(squarey.sidesinfo("4"))
-
+print(squarey.area("4"))
 rectangley = Rectangle()
 
 print(rectangley.shapeinfo("Rectangle"))
 print(rectangley.sidesinfo("4","3"))
+print(rectangley.area("4","3"))
