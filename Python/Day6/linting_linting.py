e1570e2c8a8ca6e0e084a50c885b13eab7d22154
cjfitzy <cj_fitzy@hotmail.co.uk>
2023-09-18 10:19:08 +0100

    linting

---
+++ Python/Day6/linting_linting.py
@@ -0,0 +1,9 @@
+"""writing some text up here for missing module docstring"""
+
+def count(sequence, item):
+    """ adding some text for missing function docstring"""
+    y_y = 0
+    for n_n in sequence:
+        if n_n == item:
+            y_y += 1
+    return y_y
